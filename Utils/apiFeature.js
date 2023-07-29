dfecee184b9bea4526729c43fcd1586f0e044c1f
Maushish Yadav <chamarnumber102@gmail.com>
2023-07-29 17:33:39 +0530

    Api feature done

--- Utils/apiFeature.js
+++ Utils/apiFeature.js
@@ -33,4 +33,30 @@ export const connectWallet=async()=>{
 
 }
 const fetchContract=(signerOrProvider) =>
-    new ethers.Contract(ChatAppABI)
+    new ethers.Contract(ChatAppABI,ChatAppAddress, signerOrProvider);
+export const connectingWithContract=async()=>{
+try{
+    const web3modal =new Web3Modal();
+    const connection =await web3modal.connect();
+    const provider=new ethers.providers.Web3Provider(connection);
+    const signer=provider.getSigner();
+    const Contract =fetchContract(Signer);
+
+    return Contract;
+}catch(error){
+console.log(error);
+}   
+}  
+//Converting timestamp into readable time 
+export const converTime=(time)=>{
+    const newTime=new Date(time.toNumber());
+    
+    const realTime=
+    newTIme.getHours()+ "/"
+    +newTime.getMinutes()+"/"
+    + newTime.getSeconds()+" Date:"
+    + newTime.getDate()+"/"
+    +(newTime.getMonth()+ 1)+"/"
+    newTime/getFullYear();
+    return realTime;
+}
