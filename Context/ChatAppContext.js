a18c651bc39d2b659629d3596b3e1cc1e17b1593
Maushish Yadav <chamarnumber102@gmail.com>
2023-08-03 16:18:18 +0530

    layout

--- Context/ChatAppContext.js
+++ Context/ChatAppContext.js
@@ -1,4 +1,16 @@
 import React, {useState,useEffect} from 'react'
 import { useRouter } from 'next/router'
 //Internal Import
-import{CHEC}
+import { CheckIfWalletConnected, connectWallet,connectingWithContract } from '@/Utils/apiFeature'
+
+
+export const ChatAppContext=React.createContext()
+export const ChatAppProvider=({children})=>{
+    const title="Hey Welcome to blockchain Chat App"
+
+    return (
+        <ChatAppContext.Provider value={title}>
+            {children}
+        </ChatAppContext.Provider>
+    )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ChatAppProvider } from '@/Context/ChatAppContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
